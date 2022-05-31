1be695d3ffac2e738b7ec5f4709d63d4bfff270b
Gabriel Simmons <simmons.gabe@gmail.com>
2022-05-31 13:16:58 -0700

    modify run_evaluation

--- leaderboard/scripts/run_evaluation.sh
+++ leaderboard/scripts/run_evaluation.sh
@@ -11,17 +11,17 @@ export PYTHONPATH=$PYTHONPATH:scenario_runner
 
 export LEADERBOARD_ROOT=leaderboard
 export CHALLENGE_TRACK_CODENAME=SENSORS
-export PORT=2000 # same as the carla server port
-export TM_PORT=8000 # port for traffic manager, required when spawning multiple servers/clients
+export PORT=4800 # same as the carla server port
+export TM_PORT=9000 # port for traffic manager, required when spawning multiple servers/clients
 export DEBUG_CHALLENGE=0
 export REPETITIONS=1 # multiple evaluation runs
-export ROUTES=leaderboard/data/validation_routes/routes_town05_short.xml
+export ROUTES=leaderboard/data/training_routes/routes_town06_tiny.xml
 export TEAM_AGENT=leaderboard/team_code/auto_pilot.py # agent
 export TEAM_CONFIG=aim/log/aim_ckpt # model checkpoint, not required for expert
-export CHECKPOINT_ENDPOINT=results/sample_result.json # results file
-export SCENARIOS=leaderboard/data/scenarios/no_scenarios.json
-export SAVE_PATH=data/expert # path for saving episodes while evaluating
-export RESUME=True
+export CHECKPOINT_ENDPOINT=results/results_town_06_tiny.json # results file
+export SCENARIOS=leaderboard/data/scenarios/town06_all_scenarios.json
+export SAVE_PATH=data/Town06 # path for saving episodes while evaluating
+# export RESUME=True
 
 python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
 --scenarios=${SCENARIOS}  \
@@ -36,4 +36,3 @@ python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
 --resume=${RESUME} \
 --port=${PORT} \
 --trafficManagerPort=${TM_PORT}
-
